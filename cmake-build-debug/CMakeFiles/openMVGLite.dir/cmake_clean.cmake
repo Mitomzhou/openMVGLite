b5ab69d2d0e3ea7ee89a6ea5cc697e56a6e1ebf2
Mitomzhou <zhouyong_monky@163.com>
2021-09-22 21:34:21 +0800

    编写cmake

---
+++ cmake-build-debug/CMakeFiles/openMVGLite.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openMVGLite.dir/main.cpp.o"
+  "openMVGLite"
+  "openMVGLite.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openMVGLite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
